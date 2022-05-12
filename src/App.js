30993d0528cca0672646375d396bed32c24fe6cd
Neil Bennett <neil.bennett876@Neils-MacBook-Air.local>
2022-05-12 17:00:04 -0400

    my first react project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("Manny")
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const loginLogout = () => setIsLoggedIn(!isLoggedIn)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>You clicked the button {count} times.</h1>
+        <button onClick={()=>setCount(count + 1)}>+</button>
+        <button onClick={()=>setCount(0)}>reset</button>
+        <button onClick={()=>setCount(count ? count -1 : 0)}>-</button>
+        <button onClick={()=>setCount(Math.max(count -1,0))}>-</button>
+
+        <h2>Welcome {isLoggedIn ? "back" : "guest"}</h2>
+        <button onClick={loginLogout}>{isLoggedIn ? "Logout👋🏾" : "Login"}</button>
+
+      
+      <section>
+        <h2 onClick={()=>setFirstName("Todd")}>Hello {firstName}.</h2>
+      </section>
       </header>
     </div>
   );
